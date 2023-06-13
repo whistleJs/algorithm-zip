ab98cca706d1121cd27d24f4bc73dd15cb6fef39
정성휘 <whistle_js@naver.com>
2023-06-14 00:58:50 +0900

    Solve: 두 수의 차
    - Programmers > Javascript > Level0

---
+++ Programmers/Javascript/Level0/두 수의 차/README.md
@@ -0,0 +1,12 @@
+# 두 수의 차
+
+> https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+### 문제 설명
+
+정수 `num1`과 `num2`가 주어질 때, `num1`에서 `num2`를 뺀 값을 `return`하도록 `soltuion` 함수를 완성해주세요.
+
+### 제한사항
+
+- -50000 ≤ `num1` ≤ 50000
+- -50000 ≤ `num2` ≤ 50000
---
+++ Programmers/Javascript/Level0/두 수의 차/solve.test.ts
@@ -0,0 +1,35 @@
+import { TestCase } from '@util/model';
+
+import solution from './solve';
+
+type TestCaseInput = [number, number];
+type TestCaesAnswer = number;
+
+const TEST_CASES: TestCase<TestCaseInput, TestCaesAnswer>[] = [
+  {
+    input: [2, 3],
+    answer: -1,
+    description: `
+      [입출력 예 #1]\n\n
+      
+      num1이 2이고 num2가 3이므로 2 - 3 = -1을 return합니다.
+    `,
+  },
+  {
+    input: [100, 2],
+    answer: 98,
+    description: `
+      [입출력 예 #2]\n\n
+      
+      num1이 100이고 num2가 2이므로 100 - 2 = 98을 return합니다.
+    `,
+  },
+];
+
+describe("두 수의 차", () => {
+  TEST_CASES.forEach(({ answer, description, input }) => {
+    test(description, () => {
+      expect(solution(...input)).toEqual(answer);
+    });
+  });
+});
