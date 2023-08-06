b2373e3ecc9815edd2c253653dc625b259a0d92c
정성휘 <whistle_js@naver.com>
2023-08-06 17:53:31 +0900

    Fix: 9로 나눈 나머지
    - Jest BigInt Error 해결

--- Programmers/Javascript/Level0/9로 나눈 나머지/solve.ts
+++ Programmers/Javascript/Level0/9로 나눈 나머지/solve.ts
@@ -1,5 +1,11 @@
-const solution = (number: number) => {
-  return BigInt(number) % 9n;
-};
+// Jest - BigInt Error
+// const solution = (number: number) => {
+//   return BigInt(number) % 9n;
+// };
+
+// Other Solution
+const solution = (number) => {
+  return [...number.toString()].reduce((acc, n) => acc + Number(n), 0) % 9;
+}
 
 export default solution;
